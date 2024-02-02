1f333f0eac12362ecb60cf4ece40b64d4368c7ac
anamindful <anamindful@gmail.com>
2024-02-01 22:41:12 -0600

    Springboot added

---
+++ src/main/java/portfolio/portfolio/AnastasiaportfolioApplication.java
@@ -0,0 +1,13 @@
+package portfolio.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnastasiaportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnastasiaportfolioApplication.class, args);
+	}
+
+}
